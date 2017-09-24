7f440e1be06bf11f13568341014cab6471cba7e3
Pierre-Lefevre <lefevre.pierre.m.d@gmail.com>
2017-09-24 17:15:23 +0200

    Add form's query and the list of facts.

--- app/server.js
+++ app/server.js
@@ -1,5 +1,6 @@
 let express = require('express')
 let app = express()
+let bodyParser = require('body-parser')
 
 // Load dependency
 let solr = require('solr-client');
@@ -12,22 +13,33 @@ let client = solr.createClient({
     "core": "highway_to_the_white_house"
 });
 
-// Add a new document
-var query = client.createQuery().q({'*' : '*'});
-client.search(query,function(err,obj){
-    if(err){
-        console.log(err);
-    }else{
-        console.log(obj.response.numFound);
-    }
-});
-
+// Moteur de template
 app.set('view engine', 'ejs')
 
+// Middleware
 app.use('/assets', express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
+// Routes
 app.get('/', (request, response) => {
-    response.render('pages/index', {test: 'Salut'})
+    response.render('pages/index')
+})
+
+app.post('/', (request, response) => {
+    if (request.body.query === undefined || request.body.query === '') {
+        response.render('pages/index')
+    } else {
+        let query = client.createQuery().q(request.body.query);
+        client.search(query,function(err, obj) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(obj.response.docs)
+                response.render('pages/index', {facts: obj.response.docs})
+            }
+        });
+    }
 })
 
 app.listen(8080)
